a72865ccbb3126e4901359a1c427262b8ef7f273
ALESSANDRO VIKRAM <alexandervikram80@gmail.com>
2020-12-01 15:04:01 +0530

    basics

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,9 +2,33 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'app-server',
-    templateUrl:'./server.component.html'
+    templateUrl:'./server.component.html',
+    styles:[`
+    .online {
+        color:white;
+    }
+    `]
 })
 
 export class ServerComponent {
+
+    serverID:number=7;
+    serverStatus:string = 'offline';
+
+    constructor() {
+        this.serverStatus = (Math.random() > 0.5) ? 'online':'offline';
+    }
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'online'?'green':'red';
+    }
+
+    checkStatus() {
+        return this.serverStatus === 'online';
+    }
 
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  createStatus = 'No server was created';
+  serverName='';
+  setServer=false;
+  servers = ['vikram'];
+  
+
+  constructor() { 
+    // setTimeout(()=>{
+    //   this.allowNewServer=true;
+    // },2000)
+  }
 
   ngOnInit(): void {
+  }
+  
+  onCreate() {
+   if ((this.serverName==''||this.serverName==null)){
+      return this.allowNewServer=true;
+   }
+  }
+
+  onAdd() {
+    this.setServer=true;
+    this.servers.push(this.serverName);
+    this.createStatus = 'Server is created with name '+ this.serverName;
+  }
+
+  onUpdateServerName(event:Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onResetName() {
+    this.serverName='';
   }
 
 }
