0018c33315c38f7ce09598d3eff2f912d53b2f14
ALESSANDRO VIKRAM <alexandervikram80@gmail.com>
2020-12-01 10:07:39 +0530

    no practices

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,12 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { WarningComponent } from './WarningAlert/warning.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent,
-    WarningComponent,
-    SuccessAlertComponent
+    ServersComponent
   ],
   imports: [
     BrowserModule,
