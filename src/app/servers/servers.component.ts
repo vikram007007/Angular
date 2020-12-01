9bb6cf8bbc07a63b34bbda9fc5d7d19a7af54e82
ALESSANDRO VIKRAM <alexandervikram80@gmail.com>
2020-12-01 15:37:51 +0530

    doubts

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
+  dispStatus=false;
+  dispArrs:number[]=[1];
+  incN=1;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  toggleDisplay() {
+    this.incN+=1;
+    this.dispArrs.push(this.incN);
+    if(this.dispStatus==false){
+      this.dispStatus=true;
+    }
+    else {
+      this.dispStatus=false;
+    }
   }
 
 }
